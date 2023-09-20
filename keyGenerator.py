3492ac55644b6e236ab56f3eb1fb634a95363487
Kathryn Kaelin <kkaelin@cub.uca.edu>
2023-09-20 13:43:37 -0500

    Find e and figured out commits
    Finally figured out how to commit changes in Spyder

--- keyGenerator.py
+++ keyGenerator.py
@@ -3,18 +3,17 @@ import random
 
 def genKey():
     #Choose two distinct primes, p and q
-    p = random.randint(1, 1000)
-    q = random.randint(1, 1000)
+    p = random.randint(2, 1000)
+    q = random.randint(2, 1000)
     
     #Test if p and q are prime
     while not Fermat(p):
         if not Fermat(p):
-            p = random.randint(1, 100)
+            p = random.randint(2, 1000)
     
     while not Fermat(q):
         if not Fermat(q):
-            q = random.randint(1, 100)
-            Fermat(q)
+            q = random.randint(2, 1000)
             
     #Compute n = pq
     n = p * q
@@ -23,8 +22,14 @@ def genKey():
     phi = (p - 1) * (q - 1)
     
     #Choose e such that 1 < e < phi and e and phi are coprime
+    e = random.randint(2, phi - 1)
+    while gcd(e, phi) != 1:
+        if gcd(e, phi) != 1:
+            e = random.randint(2, phi - 1)
     
-    
+    print(e)
+    print(phi)
+    print(gcd(e, phi))
     #Find a d where ed is identical to 1 (mod phi)
     
 def Fermat(p = 137):
@@ -38,5 +43,15 @@ def Fermat(p = 137):
         return False
     else:
         return True
+    
+def gcd(a=1, b=1):
+    ''' The gcd function implements Euclid's
+    GCD algorithm to find the greatest common
+    divisor of two positive integers a and b'''
+    
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
 
 genKey()
